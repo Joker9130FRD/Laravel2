ca279724c63bfe9a50baf3e5696f341f466afa62
rinto <rinto.0130@gmail.com>
2019-10-05 05:50:38 +0000

    test commit

---
+++ practice.php
@@ -0,0 +1,44 @@
+<?php
+    $a = 3;
+    $b = 7;
+    echo "    ".$a + $b."    ";
+    
+    $array_month = ["1月","2月","3月","4月","5月","6月",
+                    "7月","8月","9月","10月","11月","12月"];
+    echo $array_month[7]."    ";
+    
+    $hello = "Hello,";
+    $name = "Rinto";
+    $world = "'s World";
+    echo $hello.$name.$world."    ";
+    
+    $tehc_boost = "tech ";
+    echo $tehc_boost."boost";
+    
+    
+    /*
+    $2018_calendar = [          //中括弧から大括弧に変更
+        "January" => "1月",
+        "February" => "2月",    //全角の読点から半角のカンマに変更
+        "March" => "3月",
+        "April" => "4月",
+        "May" => "5月",
+        "June" = "6月",         // ">"が不足していたので追加して"=>"に
+        "July" => "7月",
+        "August" => "8月",
+        "September" => "9月",
+        "October" => 10月,
+        "November" => "11月",
+        "December" => "12月"
+    ];                          //同じく大括弧に変更
+
+// 12月を表示する
+echo $2018_calendar["December"];    //連想配列のキーはダブルコーテーションで囲う
+    
+    
+    
+    
+    
+    */
+    
+?>
