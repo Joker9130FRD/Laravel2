d9914f83345ae54c531758cee437447cc701fec7
rinto <rinto.0130@gmail.com>
2019-10-14 05:37:30 +0000

    View-Layout

---
+++ edit.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width initial-scale=1">
+        <title>MyNews</title>
+    </head>
+    
+    <body>
+        @extends('layout.admin')
+        
+        @section('title', 'プロフィール')
+        
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>プロフィールの編集</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
+        
+        <h1>プロフィール情報の編集</h1>
+    </body>
+</html>　
